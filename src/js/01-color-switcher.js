8ccfef725cb757b48e75cb1cdacdb5b13f616d02
NadiiaChernokur <chiernokur5@gmail.com>
2023-09-16 16:24:13 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector("button[data-start]")
+const stopBtn = document.querySelector("button[data-stop]")
 
+startBtn.addEventListener("click", onStart);
+stopBtn.addEventListener("click", onStop);
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+
+function  onStart() {
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+        startBtn.setAttribute('disabled', "");
+    }, 1000) 
+    
+};
+
+
+function onStop(){
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled', "");
+    
+     document.body.style.background = "white";
+   
+};
